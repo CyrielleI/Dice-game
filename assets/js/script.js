6975541409e7d921ba44e9119a5a1c670f3d12e4
CyrielleI <cyrielle.imbo@reseau-canope.fr>
2022-04-20 08:51:24 +0200

    app improvement

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,61 +9,74 @@ var globalPlayer1 = 0;
 var currentPlayer2 = 0;
 var globalPlayer2 = 0;
 
-alert("Règles du jeu : \n Le jeu comprend 2 joueurs sur un seul et même écran. \n Chaque joueur possède un score temporaire (current) et un score global.\n À chaque tour, le joueur a son ROUND initialisé à 0 et peut lancer un dé autant de fois qu'il le souhaite. Le résultat d’un lancer est ajouté au ROUND.\n Lors de son tour, le joueur peut décider à tout moment de:\n - Cliquer sur l’option “Hold”, qui permet d’envoyer les points du ROUND vers le GLOBAL. Ce sera alors le tour de l’autre joueur. \n - Lancer le dé. S’il obtient un 1, son score ROUND est perdu et c’est la fin de son tour.\n Le premier joueur qui atteint les 100 points sur global gagne le jeu.")
+//modal rules
+displayModalRules();
 
+document.querySelector('.btn-play').addEventListener('click', () => {
+    hideModalRules();
+})
+
+//modal winner
+document.querySelector('.btn-winner').addEventListener('click', () => {
+    hideModalWinner();
+})
+
+//event roll dice
 rollDice.addEventListener('click', ()=> {
     var randomNumber = Math.floor(Math.random()*6) + 1;
     dice.innerHTML = '<img src="assets/images/'+randomNumber+'.jpg" alt="dice"/>';
     var activePlayer = document.querySelector('.player-active');
     if (randomNumber === 1) {
-        currentPlayer1 = 0;
-        document.getElementById('score-player-1').innerHTML=currentPlayer1;
-        switchActive();
         resetCurrentPoints();
+        switchActive();
     } else {
         addPointsToCurrent(activePlayer.id, randomNumber);
     }
 });
 
+//event point to global
 hold.addEventListener('click',()=> {
     var activePlayer = document.querySelector('.player-active');
-    if (activePlayer.id === 'player1'){
-    globalPlayer1 += currentPlayer1;
-    document.getElementById('global-score-player-1').innerHTML=globalPlayer1;
-    switchActive();
-    resetCurrentPoints();
-    winner(); 
+    if (activePlayer.id === 'player1') {
+        globalPlayer1 += currentPlayer1;
+        document.getElementById('global-score-player-1').innerHTML = globalPlayer1;
+        switchActive();
+        console.log ('event hold'); 
     } else {
-    globalPlayer2 += currentPlayer2;
-    document.getElementById('global-score-player-2').innerHTML=globalPlayer2;
-    switchActive();
+        globalPlayer2 += currentPlayer2;
+        document.getElementById('global-score-player-2').innerHTML = globalPlayer2;
+        checkWinner(); 
+        console.log ('event hold');
+    }
     resetCurrentPoints();
-    winner(); 
-    }  
+    checkWinner();
 });
 
+//event play new game
 newGame.addEventListener('click', ()=> {
     playNewGame();  
 });
 
+//  start of functions of application
 function playNewGame() {
-    currentPlayer1 = 0;
-    document.getElementById('score-player-1').innerHTML=currentPlayer1;
-    currentPlayer2 = 0;
-    document.getElementById('score-player-1').innerHTML=currentPlayer1;
-    globalPlayer1 = 0;
+    console.log('New game');
+
+    currentPlayer1 = currentPlayer2 = 0;
+    document.getElementById('score-player-1').innerHTML = currentPlayer1;
+    document.getElementById('score-player-2').innerHTML = currentPlayer2;
+
+    globalPlayer1 = globalPlayer2 = 0;
     document.getElementById('global-score-player-1').innerHTML=globalPlayer1;
-    globalPlayer2 = 0;
     document.getElementById('global-score-player-2').innerHTML=globalPlayer2;
 };
 
 function addPointsToCurrent(player, points) {
     if (player === 'player1') {
         currentPlayer1 += points;
-        document.getElementById('score-player-1').innerHTML=currentPlayer1;
+        document.getElementById('score-player-1').innerHTML = currentPlayer1;
     } else {
         currentPlayer2 += points;
-        document.getElementById('score-player-2').innerHTML=currentPlayer2;
+        document.getElementById('score-player-2').innerHTML = currentPlayer2;
     }
 };
 
@@ -78,16 +91,40 @@ function switchActive() {
 };
 
 function resetCurrentPoints() {
-    currentPlayer1 = 0;
-    document.getElementById('score-player-1').innerHTML=currentPlayer1;
-    currentPlayer2 = 0;
-    document.getElementById('score-player-2').innerHTML=currentPlayer2;
+    currentPlayer1 = currentPlayer2 = 0;
+    document.getElementById('score-player-1').innerHTML = currentPlayer1;
+    document.getElementById('score-player-2').innerHTML = currentPlayer2;
 };
 
-function winner() {
-    if (globalPlayer1 >= 100) {
-        alert('Player 1 win with ' + globalPlayer1 +' points!');
-    } else if (globalPlayer2 >= 100) {
-        alert('Player 2 win with ' + globalPlayer2 +' points!');
+function checkWinner() {
+    if (globalPlayer1 >= 10) {
+        console.log ('function winner');
+        displayModalWinner('Player 1 wins the game !');
+    } else if (globalPlayer2 >= 10) {
+        console.log ('function winner');
+        displayModalWinner('Player 2 wins the game !');
     }
 }
+//end of functions of application
+
+//start functions of rules modal
+function displayModalRules() {
+    document.querySelector('#js-modal').classList.add('bg-active');
+}
+
+function hideModalRules() {
+    document.querySelector('#js-modal').classList.remove('bg-active');
+}
+//end functions of rules modal
+
+//start functions of rules winner
+function displayModalWinner(message) {
+    console.log('function displaymodalwinner');
+    document.querySelector('#js-modal-winner').classList.add('bg-active-winner');
+    document.querySelector('#js-text').innerHTML = message;
+}
+
+function hideModalWinner() {
+    document.querySelector('#js-modal-winner').classList.remove('bg-active-winner');
+}
+//end functions of rules winner
